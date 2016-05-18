317b2efb32ba6cdd85e3ce27cbc8c2f746c84bfc
mrchezco1995 <mrchezco1995@gmail.com>
2016-05-18 19:12:34 +0800

    added personal stuffs

--- Client/about.py
+++ Client/about.py
@@ -11,31 +11,31 @@ class About:
         self.imagelabel = Label(self.root, image=self.imagelogo)
         self.imagelabel.place(x=13, y=13)
 
-        self.label0 = Label(self.root, text="Python Project Submitted By:", font=("Segoe UI", 14), fg="Black")
+        self.label0 = Label(self.root, text="PyChat By:", font=("Segoe UI", 14), fg="Black")
         self.label0.place(x=9, y=116)
 
-        self.label1 = Label(self.root, text="Bernardino, Precious Lea", font=("Segoe UI", 12), fg="Black")
+        self.label1 = Label(self.root, text="Renz Hernandez (mrchezco1995)", font=("Segoe UI", 12), fg="Black")
         self.label1.place(x=13, y=152)
 
-        self.label2 = Label(self.root, text="Camballa, Queenie", font=("Segoe UI", 12), fg="Black")
+        self.label2 = Label(self.root, text="Follow me on Twitter: @TechKidPH", font=("Segoe UI", 12), fg="Black")
         self.label2.place(x=13, y=182)
 
-        self.label3 = Label(self.root, text="De Jesus, Paula Bianca", font=("Segoe UI", 12), fg="Black")
+        self.label3 = Label(self.root, text="Follow me on FB: fb.com/MrChezco1995r2", font=("Segoe UI", 12), fg="Black")
         self.label3.place(x=13, y=212)
 
-        self.label4 = Label(self.root, text="Hernandez, Franchezco Renz", font=("Segoe UI", 12), fg="Black")
+        self.label4 = Label(self.root, text="Sub to my YouTube Channels down below", font=("Segoe UI", 12), fg="Black")
         self.label4.place(x=13, y=242)
 
-        self.label5 = Label(self.root, text="Larriestan, Henry", font=("Segoe UI", 12), fg="Black")
+        self.label5 = Label(self.root, text="youtube.com/user/mrchezco1995", font=("Segoe UI", 12), fg="Black")
         self.label5.place(x=13, y=272)
 
-        self.label6 = Label(self.root, text="Mendoza, Jovelyn", font=("Segoe UI", 12), fg="Black")
+        self.label6 = Label(self.root, text="youtube.com/user/TechKidPH", font=("Segoe UI", 12), fg="Black")
         self.label6.place(x=13, y=302)
 
-        self.label6 = Label(self.root, text="Ramos, Jantrick", font=("Segoe UI", 12), fg="Black")
+        self.label6 = Label(self.root, text="HAIL HYDRA!!!", font=("Segoe UI", 12), fg="Black")
         self.label6.place(x=13, y=332)
 
-        self.label7 = Label(self.root, text="Ridon, Princess Diana", font=("Segoe UI", 12), fg="Black")
+        self.label7 = Label(self.root, text="jk XD ", font=("Segoe UI", 12), fg="Black")
         self.label7.place(x=13, y=362)
 
         self.btnOkay = Button(self.root, text="Okay", command=self.root.destroy)
